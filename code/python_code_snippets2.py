ca02eb04d3a78c74bc0e47052a7eecabff58145e
jazzapple <jpl.chong@gmail.com>
2022-11-07 17:59:11 +1100

    npzero

--- code/python_code_snippets2.py
+++ code/python_code_snippets2.py
@@ -404,6 +404,22 @@ print('a:', a)
 print('\nb:', b)
 print('\nnp.c_[a,b]:\n ',np.c_[a, b])
 
+# Searching arrays with np.nonzero
+# np.nonzero returns a tuple of two NumPy arrays. 
+# The first array gives the row indices of non-zero elements. 
+# The second array gives the column indices of non-zero elements.
+## Data: air quality index AQI data (row = city)
+X = np.array(
+[[ 42, 40, 41, 43, 44, 43 ], # Hong Kong
+[ 30, 31, 29, 29, 29, 30 ], # New York
+[ 8, 13, 31, 11, 11, 9 ], # Berlin
+[ 11, 11, 12, 13, 11, 12 ]]) # Montreal
+cities = np.array(["Hong Kong", "New York", "Berlin",
+"Montreal"])
+# Find cities with above average pollution
+polluted = set(cities[np.nonzero(X > np.average(X))[0]])
+print(polluted)
+
 ##############
 # Anaconda
 ##############
